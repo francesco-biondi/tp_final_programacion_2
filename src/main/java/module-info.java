987e623f0553c9534d09f5c15a030014ccdb5ebd
francesco-biondi <francesco_biondi@hotmail.com>
2024-09-21 17:26:10 -0300

    Creating project folder.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.tpfinalprogramacion2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.tpfinalprogramacion2 to javafx.fxml;
+    exports com.tpfinalprogramacion2;
+}
