6e823cfb88eaac51b5d99cd1bb585be237798fa2
francesco-biondi <francesco_biondi@hotmail.com>
2024-09-21 21:26:19 -0300

    Configured the project structure

--- src/main/java/com/tpfinalprogramacion2/App.java
+++ src/main/java/com/tpfinalprogramacion2/App.java
@@ -3,6 +3,8 @@ package com.tpfinalprogramacion2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +12,13 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Font.loadFont(getClass().getResourceAsStream("/assets/fonts/ONE PIECE.ttf"), 70);
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("mainMenu-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("One Piece!");
+        Image icon = new Image("file:src/main/resources/assets/icons/opf_icon.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("One Piece");
+        stage.setFullScreen(true);
         stage.setScene(scene);
         stage.show();
     }
