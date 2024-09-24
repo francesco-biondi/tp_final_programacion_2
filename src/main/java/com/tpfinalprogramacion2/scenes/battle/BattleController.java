7d143a577171df4bab079fa87cb4fc605263d39d
“Iviux” <francesco_biondi@hotmail.com>
2024-09-24 15:33:26 -0300

    Add transition to the changeScene method

--- src/main/java/com/tpfinalprogramacion2/scenes/battle/BattleController.java
+++ src/main/java/com/tpfinalprogramacion2/scenes/battle/BattleController.java
@@ -1,4 +1,55 @@
 package com.tpfinalprogramacion2.scenes.battle;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+
 public class BattleController {
+
+    @FXML
+    private ImageView ability_bar_image;
+
+    @FXML
+    private StackPane button_1;
+
+    @FXML
+    private StackPane button_2;
+
+    @FXML
+    private StackPane button_3;
+
+    @FXML
+    private StackPane button_4;
+
+    @FXML
+    private ImageView enemyImage;
+
+    @FXML
+    private ProgressBar health_bar;
+
+    @FXML
+    private ImageView pistolImage;
+
+    @FXML
+    void attack(MouseEvent event) {
+
+    }
+
+    @FXML
+    void button_pressed(MouseEvent event) {
+
+    }
+
+    @FXML
+    void fist_cursor(MouseEvent event) {
+
+    }
+
+    @FXML
+    void reset_cursor(MouseEvent event) {
+
+    }
+
 }
