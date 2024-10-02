1c64afa32fbf8c34cdc618f76dde31c518bef018
“Iviux” <francesco_biondi@hotmail.com>
2024-10-02 12:57:08 -0300

    added goldProperty to player

--- src/main/java/game/models/characters/Player.java
+++ src/main/java/game/models/characters/Player.java
@@ -1,17 +1,20 @@
 package game.models.characters;
 
 import game.models.abilities.Ability;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 
 import java.util.ArrayList;
 
 public class Player extends Character{
 
     private int gold = 0;
+    private transient IntegerProperty goldProperty;
 
     ArrayList<Ability> abilities = new ArrayList<>();
     ArrayList<Nakama> nakamas = new ArrayList<>();
 
-    public Player(String name, double bounty, String image, int gold, ArrayList<Ability> abilities, ArrayList<Nakama> nakamas) {
+    public Player(String name, double bounty, String image, ArrayList<Ability> abilities, ArrayList<Nakama> nakamas) {
         super(name, bounty, image);
         this.abilities = abilities;
         this.nakamas = nakamas;
@@ -23,5 +26,10 @@ public class Player extends Character{
 
     public void setGold(int gold) {
         this.gold = gold;
+        this.goldProperty.set(gold);
+    }
+
+    public IntegerProperty goldProperty() {
+        return goldProperty == null ? goldProperty  = new SimpleIntegerProperty(gold) : goldProperty;
     }
 }
