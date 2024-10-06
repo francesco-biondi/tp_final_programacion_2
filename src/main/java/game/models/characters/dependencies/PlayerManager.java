1fb9f62635e407560e0632f465ce6a25feabbc48
MatyBene <benedettimaty@gmail.com>
2024-10-06 16:25:04 -0300

    create applyAbility method in PlayerManager

--- src/main/java/game/models/characters/dependencies/PlayerManager.java
+++ src/main/java/game/models/characters/dependencies/PlayerManager.java
@@ -1,5 +1,8 @@
 package game.models.characters.dependencies;
 
+import game.models.abilities.Ability;
+import game.models.abilities.AttackAbility;
+import game.models.abilities.BuffAbility;
 import game.models.characters.Enemy;
 import game.models.characters.Player;
 
@@ -9,4 +12,14 @@ public class PlayerManager {
         int goldByClick = enemy.getGOLD_BY_CLICK();
         player.setGold(goldByClick);
     }
+
+    private static void applyAbility(Player player, Enemy enemy, Ability ability){
+        if(ability instanceof AttackAbility){
+            double damage = ability.use(enemy);
+        } else if(ability instanceof BuffAbility){
+            double buffDamage = ability.use(player);
+        }
+    }
+
+    
 }
