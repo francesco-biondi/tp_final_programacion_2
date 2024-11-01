a5ade625ba0779d21011afa74e221a028985f7aa
MatyBene <benedettimaty@gmail.com>
2024-10-31 22:21:07 -0300

    setUp

--- src/test/java/game/models/abilities/NakamaTest.java
+++ src/test/java/game/models/abilities/NakamaTest.java
@@ -1,7 +1,37 @@
 package game.models.abilities;
 
+import game.DataProvider;
+import game.models.abilities.enums.NakamaNames;
+import game.models.characters.Enemy;
+import game.models.characters.Player;
+import game.models.saves.Save;
+import game.scenes.dependencies.GameManager;
+import org.junit.jupiter.api.BeforeEach;
+
 /**
  * Clase de prueba para la clase Nakama
  */
 public class NakamaTest {
+
+    private Save testSave;
+    private Player testPlayer;
+    private Enemy testEnemy;
+    private Nakama testNakama;
+
+    /**
+     * Configura el entorno de prueba antes de cada test.
+     * Inicializacia un saveMock, un player, un enemy y un nakama.
+     */
+    @BeforeEach
+    public void setUp() {
+
+        testSave = DataProvider.saveMock();
+        GameManager.setCurrentSave(testSave);
+
+        testPlayer = GameManager.getCurrentPlayer();
+        testEnemy = testSave.getEnemy(0);
+
+        testNakama = testPlayer.getNakama(NakamaNames.ZORO);
+    }
+    
 }
