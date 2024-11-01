5273331cfc6ac1665e4d4753a313dbf205830928
MatyBene <benedettimaty@gmail.com>
2024-10-31 22:16:16 -0300

    setUp

--- src/test/java/game/models/abilities/AttackAbilityTest.java
+++ src/test/java/game/models/abilities/AttackAbilityTest.java
@@ -1,7 +1,38 @@
 package game.models.abilities;
 
+import game.DataProvider;
+import game.models.abilities.enums.AbilityNames;
+import game.models.characters.Enemy;
+import game.models.characters.Player;
+import game.models.saves.Save;
+import game.scenes.dependencies.GameManager;
+import org.junit.jupiter.api.BeforeEach;
+
 /**
  * Clase de prueba para la clase AttackAbility
  */
 public class AttackAbilityTest {
+
+    private Save testSave;
+    private Player testPlayer;
+    private Enemy testEnemy;
+    private AttackAbility testAA;
+
+    /**
+     * Configura el entorno de prueba antes de cada test.
+     * Inicializacia un saveMock, un player, un enemy y una attack ability.
+     */
+    @BeforeEach
+    public void setUp(){
+
+        testSave = DataProvider.saveMock();
+        GameManager.setCurrentSave(testSave);
+
+        testPlayer = GameManager.getCurrentPlayer();
+        testEnemy = testSave.getEnemy(0);
+
+        testAA = (AttackAbility) testPlayer.getAbility(AbilityNames.PISTOL);
+    }
+
+
 }
